322f97a4d40224ab0c3019b6ccbe969d8d41a0d1
Tarvo Erimäe <tarvo.erimae@gmail.com>
2017-09-25 01:16:58 +0300

    Esimene vahetähtaeg

---
+++ src/test/java/WeatherAPITests.java
@@ -0,0 +1,61 @@
+
+import org.junit.Test;
+import java.net.URL;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+public class WeatherAPITests {
+
+    @Test
+    public void doesReturnForecastUrl() throws Exception {
+
+        //Näidissisend forecastUrlile
+        final String forecastUrl = "tere";
+
+        //Testib kas forecastUrl on URL klassist
+        assertThat(forecastUrl, instanceOf(URL.class));
+    }
+
+    @Test
+    public void doesConnectToAPI() throws Exception {
+
+        // Siia pärast : openWeatherApi.getStatusCode vms mis tagastab lehekülje staatuse
+        final Integer responseStatusCode = 2;
+
+        int statusCode = responseStatusCode;
+        assertEquals(statusCode, 200);
+    }
+
+    @Test
+    public void doesReturnThreeDayForecast() throws Exception {
+
+        // Siia pärast: openWeatherApi.getThreeDayWeather vms, mis tagastab json listi ja tuleb leida listi lenght
+        final JSONArray threeDayForecastResponse = 1;
+
+        int numberOfForecastsInJsonList = threeDayForecastResponse.length();
+        assertEquals(3, numberOfForecastsInJsonList);
+
+    }
+
+    @Test
+    public void doesReturnHighestAndLowestTemperatureOfEachDay() throws Exception {
+
+        //Siia pärast: openWeatherApi.getHighestAndLowestTemperaturesForEachDay vms, mis tagastab kõrgima ja madalaima temperatuuri
+        //TODO: Implement code
+
+    }
+
+    @Test
+    public void doesReturnGeographicalCoordinates() throws Exception {
+
+        //Siia pärast: openWeatherApi.getGeographicalCoordinates vms, mis tagastab linna koordinaadid
+        final JSONObject coordinatesResponse = openWeatherAPI.getGeoCoordsOfRequestedCity(countryCode, city, key);
+        Boolean responseHasLatitude = coordinatesResponse.has("lat");
+        Boolean responseHasLongitude = coordinatesResponse.has("lon");
+        Boolean hasLatitudeAndLongitude = responseHasLatitude && responseHasLongitude;
+        assertEquals(true, hasLatitudeAndLongitude);
+    }
+
+}
+
