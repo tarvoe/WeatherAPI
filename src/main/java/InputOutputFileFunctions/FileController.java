418ea87f222b005eb3b812b3ad2d70bcb1ba9053
Tarvo Erimäe <tarvo.erimae@gmail.com>
2017-12-02 15:24:27 +0200

    03.01 tehtud muudatused
    Faili kirjutamise funktsionaalsus
    
    input.txt failis võib olla ükskõik kui palju linnasid

--- src/main/java/InputOutputFileFunctions/FileController.java
+++ src/main/java/InputOutputFileFunctions/FileController.java
@@ -1,6 +1,8 @@
 package InputOutputFileFunctions;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -11,8 +13,7 @@ public class FileController {
     public static void main(String[] args) throws IOException {
 
         FileController reader = new FileController();
-        String file = reader.filePathConstructor("input.txt");
-        System.out.println(reader.fileReader(file).get(2));
+        reader.createNewTxtFile("Tallinn3");
     }
 
     public String filePathConstructor (String filenameWithExtention) {
@@ -25,4 +26,31 @@ public class FileController {
         return lines;
     }
 
+    public void createNewTxtFile (String cityName) throws IOException {
+        try {
+            File file = new File("C:\\Users\\Tarvo\\IdeaProjects\\"+cityName+".txt");
+            boolean fvar =file.createNewFile();
+            if (fvar){
+                System.out.println("File has been created successfully");
+            }
+            else{
+                System.out.println("File already present at the specified location");
+            }
+        } catch (IOException exception) {
+            System.out.println("Something went wrong");
+            exception.printStackTrace();
+        }
+    }
+
+    public  void fileWriter(List<String> content, String cityName ) throws IOException {
+        String directory = "C:\\Users\\Tarvo\\IdeaProjects\\";
+        FileWriter fw = new FileWriter(new File(directory, cityName+".txt"));
+        String newLine = System.getProperty("line.separator");
+        fw.write("The maximum and minimum temperatures in " + cityName + " for the next three days are" + newLine);
+        for (int i = 0; i < content.size(); i++) {
+            fw.write(content.get(i) + newLine);
+        }
+
+        fw.close();
+    }
 }
--- src/main/java/MainFunctions/OpenWeatherAPI.java
+++ src/main/java/MainFunctions/OpenWeatherAPI.java
@@ -1,11 +1,16 @@
 package MainFunctions;
 
 
+import InputOutputFileFunctions.FileController;
+import InputOutputFileFunctions.UserInputController;
 import ResponseFunctions.ResponseController;
+import UrlRequests.UrlBuilder;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.io.IOException;
+import java.net.URL;
 import java.util.*;
 
 public class OpenWeatherAPI implements WeatherInterface {
@@ -13,6 +18,36 @@ public class OpenWeatherAPI implements WeatherInterface {
     private static final Double INITIAL_VALUE_FOR_MINIMUM_TEMPERATURE = 200.0;
     private static final Double INITIAL_VALUE_FOR_MAXIMUM_TEMPERATURE = -150.0;
     private static ResponseController responseController = new ResponseController();
+    private static String APPID = "0786e4e1ae01d4e119f0260e53a683d0";
+
+    public static void main(String[] args) throws IOException, JSONException {
+        FileController fileController = new FileController();
+        UserInputController userInputController = new UserInputController();
+        UrlBuilder urlBuilder = new UrlBuilder();
+        ResponseController responseController = new ResponseController();
+        OpenWeatherAPI openWeatherAPI = new OpenWeatherAPI();
+
+        List<String> cities = userInputController.userInputTaker();
+
+        for (int i=0 ; i<cities.size(); i++) {
+            URL foreCastUrl = urlBuilder.buildNewForecastRequestURL(cities.get(i), APPID);
+            String responseBody = responseController.getResponseBodyFromURL(foreCastUrl);
+            JSONObject responseInJSON = responseController.makeStringResponseToJSONObject(responseBody);
+            String cityName = responseController.getCityName(responseInJSON);
+
+            HashMap<String, Object> threeDays = openWeatherAPI.createHashMapOfThreeDayForecast(responseInJSON);
+            System.out.println(threeDays.toString());
+
+            ArrayList<Double> latandlon = openWeatherAPI.getLanLotOfCityFromUrlResponseInArrayList(responseInJSON);
+
+            List <String> content = new ArrayList<>();
+            content.add(threeDays.toString());
+            content.add(latandlon.toString());
+            fileController.fileWriter(content, cityName);
+        }
+
+
+    }
 
     @Override
     public HashMap<String, Object> createHashMapOfThreeDayForecast (JSONObject dataInJSONFormat) throws JSONException {
@@ -51,7 +86,7 @@ public class OpenWeatherAPI implements WeatherInterface {
                 minTemperatuur = INITIAL_VALUE_FOR_MINIMUM_TEMPERATURE;
             }
         }
-        System.out.println("The minimum and maximum temperatures in " + responseController.getCityName(dataInJSONFormat) + " for the next three days are:");
+        System.out.println("The maximum and minimum temperatures in " + responseController.getCityName(dataInJSONFormat) + " for the next three days are:");
         return threeDaysWithTheirMinAndMaxTemperatures;
     }
 
@@ -65,7 +100,4 @@ public class OpenWeatherAPI implements WeatherInterface {
         return latandlon;
     }
 
-    public static void main(String[] args) throws IOException, JSONException {
-
-    }
 }
